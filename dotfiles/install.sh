374c15e46685b8777f0210e8ea3a4be43b91d072
José Bezerra <121698681+JoJoseB@users.noreply.github.com>
2024-03-21 15:21:42 -0300

    update files

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -10,17 +10,21 @@ slick-greeter \
 feh \
 -y
 
-mkdir -p ~/.config/{bspwm,sxhkd,polybar,rofi,picom,slick-greeter}
+mkdir -p ~/.config/{bspwm,sxhkd,polybar,rofi,picom,slick-greeter,feh}
 
 #slick-greeter copy files
 cp ./slick-greeter/lightdm.conf /home/$USER/.config/slick-greeter/
 sudo rm /etc/lightdm/lightdm.conf && sudo ln -s /home/$USER/.config/slick-greeter/lightdm.conf /etc/lightdm/
 
 #bspwm + sxhkd
-cp ./bspwm/bspwmrc /home/$USER/.config/bspwm/ && cp ./sxhkd/sxhkdrc /home/$USER/.config/sxhkd/
+chmod +x ./bspwm/bspwmrc && cp ./bspwm/bspwmrc /home/$USER/.config/bspwm/
+cp ./sxhkd/sxhkdrc /home/$USER/.config/sxhkd/
 
 #picom
 cp ./picom/picom.conf /home/$USER/.config/picom/
 
 #polybar
 cp ./polybar/config.ini /home/$USER/.config/polybar/
+
+#feh
+cp ../sources/wallpaper/wallpaper.png /home/$USER/.config/feh/
